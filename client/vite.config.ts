68718a52968eaeeca16e2e8ed54c80a68d631c88
beny2332 <bj57577@gmail.com>
2025-06-25 18:01:12 +0300

    a

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/music-sampler/',
+  base: '/music-sampler/client/',
   build: {
     outDir: 'dist'
   }
